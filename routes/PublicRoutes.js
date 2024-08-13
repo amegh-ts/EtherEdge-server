4d58971ea5fd0e5951c179b3b50154934910a9eb
amegh-ts <80452444+amegh-ts@users.noreply.github.com>
2024-08-13 21:16:25 +0530

    Add: serverStatus api

--- routes/PublicRoutes.js
+++ routes/PublicRoutes.js
@@ -5,4 +5,7 @@ const router = require("express").Router();
 // db stats
 router.get("/dbstats", getDbStats);
 
+// server status
+router.get("/serverStatus", getServerStatus);
+
 module.exports = router;
